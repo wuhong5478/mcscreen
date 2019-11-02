094fe6e8e71786440bc8d3a7b0ddb3529f6b8dfd
wuhong <530842442@qq.com>
2019-11-02 08:35:38 +0800

    样式修改

--- src/views/leftBox/equipmentCondition.vue
+++ src/views/leftBox/equipmentCondition.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="equipmentCondition">
     <div class="title"><span>设备统计</span></div>
-    <div class="content"></div>
+    <div class="content">
+		<div class="content-tips">
+			<span class="txt">已接入设备总数</span>
+			<span class="number" style="margin-right:0.5rem;">2341</span>
+			<span class="txt">设备完好率</span>
+			<span class="number">98.19%</span>
+		</div>
+	</div>
   </div>
 </template>
 <script>
@@ -16,5 +23,22 @@ export default {
 <style lang="less">
   .equipmentCondition{
     height: 100%;
+	.content{
+		.content-tips{
+			.txt{
+				font-size: 0.23rem;
+				font-weight: 700;
+				font-style: normal;
+				color: #00FFFF;
+			}
+			.number{
+				font-size: 0.23rem;
+				margin-left: 0.4rem;
+				font-weight: 700;
+				font-style: normal;
+				color: #FFFF00;
+			}
+		}
+	}
   }
 </style>
