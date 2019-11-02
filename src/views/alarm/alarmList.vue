c2f63b83611020ce6894608101a464c58d4593c6
wuhong <530842442@qq.com>
2019-11-02 08:43:27 +0800

    表格添加

--- src/views/alarm/alarmList.vue
+++ src/views/alarm/alarmList.vue
@@ -7,6 +7,32 @@
       </el-radio-group>
     </div>
     <div class="content alarmList-content">
+		<table class="tableList">
+			<!-- <tr>
+				<th>报警类型</th>
+				<th>报警时间</th>
+				<th>报警地址</th>
+				<th>报警等级</th>
+			</tr> -->
+			<tr>
+				<td>todo</td>
+				<td>2019-10-27 18:21:23（ID：1234）</td>
+				<td>梅城古镇东南南望街西北侧兰州拉面-用电安全器发生火灾故障</td>
+				<td>【重大隐患】</td>
+			</tr>
+			<tr>
+				<td>todo</td>
+				<td>2019-10-27 18:21:23（ID：1234）</td>
+				<td>梅城古镇梅城大楼—3号楼消控室，摄像头视频监控-人员离岗【图片】</td>
+				<td>【一般隐患】</td>
+			</tr>
+			<tr>
+				<td>todo</td>
+				<td>2019-10-27 18:21:23（ID：1234）</td>
+				<td>梅城古镇东南南望街西北侧兰州拉面-用电安全器发生火灾故障</td>
+				<td>【重大隐患】</td>
+			</tr>
+		</table>
     </div>
   </div>
 </template>
@@ -54,50 +80,24 @@ export default {
       font-size: .2rem;
       height: auto;
       padding-top: 0.2rem;
-
-      p {
-        display: inline-block;
-        line-height: .36rem;
-        vertical-align: bottom;
-      }
-
-      p.left {
-        width: 85%;
-        text-align: left;
-        .icon {
-          width: .22rem;
-          height: .36rem;
-          display: inline-block;
-          background-size: 100% auto !important;
-          padding-right: 0;
-          vertical-align: bottom;
-        }
-      }
-      p.center {
-        width: calc(20% - .1rem);
-        padding-left: .1rem;
-        text-align: left;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-      }
-      p.right {
-        width: 15%;
-        text-align: right;
-        vertical-align: middle;
-      }
-      span.devName {
-        color: #23AAF2;
-        cursor: pointer;
-      }
-      span.devName:hover {
-        text-decoration: underline;
-        cursor: pointer;
-      }
-      span.devName1:hover {
-        text-decoration: underline;
-        cursor: pointer;
-      }
+      .tableList{
+		 width: calc(100% - 0.3rem);
+		 margin: 0 auto;
+		 margin-top: 0.1rem;
+		 th{
+		 	line-height: 0.65rem;
+		 	text-align: left;
+		 	color: white;
+		 	font-size: 0.23rem;
+		 	
+		 }
+		 td{
+		 	line-height: 0.5rem;
+		 	text-align: left;
+		 	color: white;
+		 	font-size: 0.23rem;
+		 }
+	  }
     }
 	.raidoBox{
 		margin-left: 1rem;
