d82f742f6872ab0a0d03dbc92afa8c0c7bdf9f01
wuhong <530842442@qq.com>
2019-11-02 08:30:11 +0800

    地图组件相关

--- src/views/map/aMap.vue
+++ src/views/map/aMap.vue
@@ -1,13 +1,12 @@
 <template>
 	<div class="aMap" id="mapContainer">
-		
 		<ul class="legend">
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
+			<li>微型消防站</li>
+			<li>医院</li>
+			<li>危险源</li>
+			<li>消防室</li>
+			<li>水源</li>
+			<li>摄像头</li>
 		</ul>
 	</div>
 </template>
@@ -16,8 +15,24 @@
 	export default {
 		name: 'aMap',
 		data() {
-			return {}
+			return {
+				mapData: [
+					{
+						lon:116.38,
+						lat:39.90
+					},
+					{
+						lon:116.38,
+						lat:39.81
+					},
+					{
+						lon:116.39,
+						lat:39.83
+					}
+				]
+			}
 		},
+		
 		mounted() {
 			this.initMap();
 		},
@@ -27,27 +42,73 @@
 					center: [116.397428, 39.90923],
 					resizeEnable: true,
 					zoom: 11
-				})
+				});
+				
+				 AMapUI.loadUI(['control/BasicControl'], function(BasicControl) {
+				
+					//添加一个缩放控件
+					map.addControl(new BasicControl.Zoom({
+						position: 'lm'
+					}));
+			
+					//图层切换控件
+					map.addControl(new BasicControl.LayerSwitcher({
+						position: 'rt'
+					}));
+				});
+				
+				var markersList = [];
+				for(var i=0;i<this.mapData.length;i++){
+					var _data = this.mapData[i];
+					var marker = new AMap.Marker({
+					    position: new AMap.LngLat(_data.lon,_data.lat),
+					    offset: new AMap.Pixel(-10, -10)
+					});
+					markersList.push(marker)
+				}
+				map.add(markersList);	
+
 			},
 		}
 	};
 </script>
 <style lang="less">
+	.amap-ui-control-position-lm{
+		left:31%!important;
+		bottom:2rem!important;
+	}
+	.amap-ui-control-position-rt{
+		top:1.5rem!important;
+		right:31%!important;
+	}
 	.aMap {
 		height: 100%;
 		width: 100%;
-		.legend{
+
+		.legend {
 			position: absolute;
 			z-index: 999;
 			right: 8.7rem;
 			bottom: 3.2rem;
-			width: 3rem;
-			height: 1.5rem;
+			width: 3.3rem;
+			height: 1.7rem;
+			padding: 0.1rem 0;
 			border-radius: 0.1rem;
 			background: inherit;
 			background-color: rgba(30, 43, 70, 1);
-			opacity: 0.6;
+			opacity: 0.8;
 
+			li {
+				width: 50%;
+				float: left;
+				height: 0.5rem;
+				line-height: 0.5rem;
+				font-weight: 700;
+				font-style: normal;
+				font-size: 0.21rem;
+				text-indent: 0.45rem;
+				color: #00FFFF;
+			}
 		}
 	}
 </style>
